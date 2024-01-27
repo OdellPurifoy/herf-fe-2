7b6365807893735af1ae55a772e8a6a41de7bf88
Odell Purifoy <opurifoy@springhealth.com>
2024-01-27 17:45:54 -0500

    Functional and styled login page with redirect

---
+++ app/components/LandingPageFooter.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+export default function LandingPageFooter() {
+  return (
+    <Box p={4} bg="white" color="gray.800" textAlign="center">
+      <p>&copy; HERF 2024. All rights reserved.</p>
+    </Box>
+  )
+}
