5fd761b640e8c4d8215dc7246be0676ca448ce97
Odell Purifoy <opurifoy@springhealth.com>
2024-02-18 19:45:31 -0500

    Added modal feature copy to three of the 6 cards

--- app/components/feature-cards/BuildYourLoungeFeatureCard.jsx
+++ app/components/feature-cards/BuildYourLoungeFeatureCard.jsx
@@ -1,33 +1,114 @@
 import {
+  Box,
   Card,
   CardBody,
   CardFooter,
+  Center,
   Stack,
   Heading,
   Button,
   Text,
 } from '@chakra-ui/react'
-import { EditIcon } from '@chakra-ui/icons'
+import { EditIcon, CheckIcon } from '@chakra-ui/icons'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 const BuildYourLoungeFeatureCard = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Card maxW="sm" variant="outline">
-      <CardBody>
-        <EditIcon w={10} h={10} color="orange.500" />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">Build Your Lounge</Heading>
-          <Text>
-            Create a virtual profile of your lounge. Feel free to include
-            details such as your address, contact information, bio and images!
-          </Text>
-        </Stack>
-      </CardBody>
-      <CardFooter>
-        <Button variant="solid" colorScheme="gray">
-          Learn More
-        </Button>
-      </CardFooter>
-    </Card>
+    <>
+      <Card maxW="sm" variant="outline">
+        <CardBody>
+          <EditIcon w={10} h={10} color="orange.500" />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Build Your Lounge</Heading>
+            <Text>
+              Create a virtual profile of your lounge. Feel free to include
+              details such as your address, contact information, bio and images!
+            </Text>
+          </Stack>
+        </CardBody>
+        <CardFooter>
+          <Button variant="solid" colorScheme="gray" onClick={onOpen}>
+            Learn More
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <Center>
+            <EditIcon w={10} h={10} mt="10" color="orange.500" />
+          </Center>
+          <ModalHeader>
+            Elevate Your Lounge's Presence with a Bespoke Profile
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Description: </b>
+              <br />
+              <p>
+                Attention cigar lounge owners! Are you ready to showcase your
+                establishment to a broader audience and captivate cigar
+                enthusiasts worldwide? Look no further than our platform's
+                premium feature: the ability to craft a tailor-made profile for
+                your lounge.
+              </p>
+              <Box mb={4} />
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Craft Your Identity: </b>
+              <br />
+              <p>
+                Take control of your lounge's online presence by crafting a
+                profile that reflects its unique ambiance, offerings, and
+                personality. From elegant decor to unparalleled cigar
+                selections, highlight what sets your lounge apart and entices
+                visitors to step into your world.
+              </p>
+              <Box mb={4} />
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Engage Your Audience: </b>
+              <br />
+              <p>
+                Capture the attention of cigar aficionados near and far with
+                captivating visuals and compelling descriptions. Showcase your
+                lounge's finest features, from cozy seating areas to exclusive
+                events, enticing visitors to experience the luxury and
+                camaraderie your lounge offers.
+              </p>
+              <Box mb={4} />
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Start Building Your Profile Today: </b>
+              <br />
+              <p>
+                Unlock the potential of your lounge and join our community of
+                esteemed cigar destinations. Sign up today to create a profile
+                that elevates your lounge's presence, captivates cigar
+                enthusiasts, and solidifies your status as a premier destination
+                for luxury and relaxation.
+              </p>
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="gray" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
--- app/components/feature-cards/TextFeatureCard.jsx
+++ app/components/feature-cards/TextFeatureCard.jsx
@@ -1,4 +1,7 @@
+'use client'
+
 import {
+  Box,
   Card,
   CardBody,
   CardFooter,
@@ -6,29 +9,118 @@ import {
   Heading,
   Button,
   Text,
+  Center,
 } from '@chakra-ui/react'
-import { PhoneIcon } from '@chakra-ui/icons'
+import { CheckIcon, PhoneIcon } from '@chakra-ui/icons'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 const TextFeatureCard = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Card maxW="sm" variant="outline">
-      <CardBody>
-        <PhoneIcon w={10} h={10} color="orange.500" />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">Text Notifications</Heading>
-          <Text>
-            Herf's text message notification service offers all the benefits of
-            email in a text message format. All Event and Special Offer details,
-            updates and cancellations are sent to your members device.
-          </Text>
-        </Stack>
-      </CardBody>
-      <CardFooter>
-        <Button variant="solid" colorScheme="gray">
-          Learn More
-        </Button>
-      </CardFooter>
-    </Card>
+    <>
+      <Card maxW="sm" variant="outline">
+        <CardBody>
+          <PhoneIcon w={10} h={10} color="orange.500" />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Text Notifications</Heading>
+            <Text>
+              Herf's text message notification service offers all the benefits
+              of email in a text message format. All Event and Special Offer
+              details, updates and cancellations are sent to your members
+              device.
+            </Text>
+          </Stack>
+        </CardBody>
+        <CardFooter>
+          <Button variant="solid" colorScheme="gray" onClick={onOpen}>
+            Learn More
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <Center>
+            <PhoneIcon w={10} h={10} mt="10" color="orange.500" />
+          </Center>
+          <ModalHeader>
+            Enhance Member Engagement and Loyalty with Text Notifications
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Empower Your Lounge: </b>
+              <br />
+              <p>
+                Take control of your lounge's communication strategy with our
+                text notifications feature. Easily announce upcoming events,
+                promotions, and special offers directly to your members' mobile
+                devices, ensuring maximum visibility and attendance.
+              </p>
+              <Box mb={4} />
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Streamline Event Management: </b>
+              <br />
+              <p>
+                Effortlessly manage your lounge's events with our text messaging
+                service. Notify members of event details, RSVP deadlines, and
+                last-minute changes, minimizing no-shows and ensuring a smooth
+                and successful gathering every time.
+              </p>
+              <Box mb={4} />
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Boost Member Engagement: </b>
+              <br />
+              <p>
+                Keep members actively engaged and connected to your lounge's
+                community with regular updates and exclusive perks. From VIP
+                access to new cigar releases to members-only events, our text
+                notifications will entice members to participate and stay
+                involved.
+              </p>
+              <Box mb={4} />
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Drive Loyalty and Retention: </b>
+              <br />
+              <p>
+                Build lasting relationships with your members by providing
+                value-added services and experiences. Our text messaging feature
+                strengthens member loyalty by keeping them informed, engaged,
+                and excited about the opportunities your lounge offers.
+              </p>
+              <Box mb={4} />
+              <CheckIcon color="orange.500" mr="2" />
+              <b>Unlock the Power of Text Messaging Today: </b>
+              <br />
+              <p>
+                Transform the way you connect with your members and elevate
+                their lounge experience with our text messaging service. Join
+                the ranks of leading lounge owners who prioritize member
+                engagement and loyalty. Sign up today to start reaping the
+                benefits of enhanced communication and community-building.
+              </p>
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="gray" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
